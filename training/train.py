3f807d5337ed01a39af18f773ebd8e2c9553fb57
Javon Davis <javonldavis14@gmail.com>
2017-10-20 11:54:23 -0500

    updated model to match nvidia autopilot

--- training/train.py
+++ training/train.py
@@ -24,13 +24,37 @@ for line in lines:
 X_train = np.array(images)
 y_train = np.array(measurements)
 
-from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda
+import keras
+import keras.models as models
+
+from keras.models import Sequential, Model
+from keras.layers.core import Dense, Dropout, Activation, Flatten, Reshape
+from keras.layers import BatchNormalization,Input
+from keras.layers.recurrent import SimpleRNN, LSTM
+from keras.layers.convolutional import Convolution2D
+from keras.optimizers import SGD, Adam, RMSprop
+import sklearn.metrics as metrics
+
+nrows = 160
+ncols = 320
 
 model = Sequential()
-model.add(Lambda(lambda x: x/ 255.0 - 0.5, input_shape=(160, 320, 3)))
+
+model.add(BatchNormalization(epsilon=0.001,mode=2, axis=1,input_shape=(3, nrows,ncols)))
+
+model.add(Convolution2D(24,5,5,border_mode='valid', activation='relu', subsample=(2,2)))
+model.add(Convolution2D(36,5,5,border_mode='valid', activation='relu', subsample=(2,2)))
+model.add(Convolution2D(48,5,5,border_mode='valid', activation='relu', subsample=(2,2)))
+model.add(Convolution2D(64,3,3,border_mode='valid', activation='relu', subsample=(1,1)))
+model.add(Convolution2D(64,3,3,border_mode='valid', activation='relu', subsample=(1,1)))
 model.add(Flatten())
-model.add(Dense(1))
+model.add(Dense(1164, activation='relu'))
+model.add(Dense(100, activation='relu'))
+model.add(Dense(50, activation='relu'))
+model.add(Dense(10, activation='relu'))
+model.add(Dense(1, activation='tanh'))
+
+model.summary()
 
 model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=2)
