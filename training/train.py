2dd7964367df67689b34c47f4a3afb8292698c0b
Javon Davis <javonldavis14@gmail.com>
2017-10-21 09:07:46 -0500

    updated training model

--- training/train.py
+++ training/train.py
@@ -29,9 +29,9 @@ import keras.models as models
 
 from keras.models import Sequential, Model
 from keras.layers.core import Dense, Dropout, Activation, Flatten, Reshape
-from keras.layers import BatchNormalization,Input
+from keras.layers import BatchNormalization,Input, Lambda
 from keras.layers.recurrent import SimpleRNN, LSTM
-from keras.layers.convolutional import Convolution2D
+from keras.layers.convolutional import Convolution2D, Cropping25
 from keras.optimizers import SGD, Adam, RMSprop
 import sklearn.metrics as metrics
 
@@ -40,15 +40,14 @@ ncols = 320
 
 model = Sequential()
 
-model.add(BatchNormalization(epsilon=0.001,mode=2, axis=1,input_shape=(3, nrows,ncols)))
-
+model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(nrows, ncols, 3)))
+model.add(Cropping2D(cropping=((70, 25), (0, 0))))
 model.add(Convolution2D(24,5,5,border_mode='valid', activation='relu', subsample=(2,2)))
 model.add(Convolution2D(36,5,5,border_mode='valid', activation='relu', subsample=(2,2)))
 model.add(Convolution2D(48,5,5,border_mode='valid', activation='relu', subsample=(2,2)))
 model.add(Convolution2D(64,3,3,border_mode='valid', activation='relu', subsample=(1,1)))
 model.add(Convolution2D(64,3,3,border_mode='valid', activation='relu', subsample=(1,1)))
 model.add(Flatten())
-model.add(Dense(1164, activation='relu'))
 model.add(Dense(100, activation='relu'))
 model.add(Dense(50, activation='relu'))
 model.add(Dense(10, activation='relu'))
